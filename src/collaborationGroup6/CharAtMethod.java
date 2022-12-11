d2ff66955b17013674cba23898b4bcd83bee538d
Kamuran Sahin <kamuransahin.c11@gmail.com>
2022-12-11 20:59:27 +0300

    finish test case 2

---
+++ src/collaborationGroup6/CharAtMethod.java
@@ -0,0 +1,30 @@
+package collaborationGroup6;
+
+public class CharAtMethod {
+
+    public static void main(String[] args) {
+
+        String firstName= "Wooden";
+        String lastName= "Spoon";
+
+        char first=firstName.charAt(0);
+        char last=lastName.charAt(0);
+
+        System.out.println(first+"."+last);
+    }
+
+/*
+  1. Can you explain charAt method?
+
+  The charAt() method returns the character at the specified index in a string.
+
+  The index of the first character is 0, the second character is 1, and so on.
+
+            2. Create a class to display your initials
+            initial:
+    firstName = "Wooden";
+    lastName = "Spoon";
+
+    output:
+    W.S*/
+}
